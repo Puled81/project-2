bf37d2e1942ed0054412cbc2074d2aac4fe28b8d
Puled81 <drakep@verizon.net>
2015-10-21 19:49:53 -0400

    Update Table.java

--- Table.java
+++ Table.java
@@ -1,124 +1,193 @@
-String title=  "ELASTIC COLLISIONS";
-String news=   "Use 'r' key to reset.";
-String author=  "Drake";
+//Making a Scene
+//Drake P  9/9/15 CST 112
 
+/// GLOBALS:  coordinates, speed, etc.
+float x, y;       // Position of Hero
+float dx, dy;     // Speed.
+float horizon;
+float sunX, sunY;
+float X, Y;        //Monster 1 position
+float DX, DY;       // monster 1 speed
+float coinx, coiny;
 
-float left, right, top, bottom;
-float middle;
+float score;
+float top,bottom,right,left;
 
-float cueX,  cueY,  cueDX,  cueDY;
-float redX,  redY,  redDX,  redDY;
-float yelX,  yelY,  yelDX,  yelDY;
-float bluX, bluY, bluDX, bluDY;
-
-//// SETUP:  size and table
+//// SETUP:  window size, initialization (start in middle of screen).
 void setup() {
-  size( 600, 400 );
-  left=   50;
-  right=  width-50;
-  top=    100;
-  bottom= height-50;
-  middle= left + (right-left) / 2;
+  size( 700,700);
+  horizon=  height/4;
+  x=  170;
+  y=  125;
+  dx= 0;
+  dy= 0;
+  X= random(0,700);
+  Y= random(301,700);
+  DX= random(-1,+1);
+  DY= random(-1,+1);
+  coinx= random(400,680);
+  coiny= random(450,680);
+  score= 50;
+  top= 0;
+  bottom= 700;
+  right= 700;
+  left= 0;
   //
   reset();
- }
- void reset() {
-   cueX=  left + (right-left) / 4;
-   cueY=  top + (bottom-top) / 2;
-   // Random positions.
-   redX=  random( middle,right );   redY=  random( top, bottom );
-   yelX=  random( middle,right );   yelY=  random( top, bottom );
-   bluX=  random( middle,right );   bluY=  random( top, bottom );
-   // Random speeds
-   cueDX= (0);  cueDY= (0);
-   redDX=  random( 1,3 );   redDY=  random( 1,3 );
-   yelDX=  random( 1,3 );   redDY=  random( 1,3 );
-   bluDX=  random( 1,3 );   bluDY=  random( 1,3 );
- }
- //// NEXT FRAME:  table, bounce off walls, collisions, show all
+}
+void reset() {
+  x=  170;
+  y=  125;
+  dx= .75;
+  dy= .75;
+  X= random(0,680);
+  Y= random(301,680);
+  DX= random(-3,+3);
+  DY= random(-3,+3);
+  coinx= random(400,680);
+  coiny= random(450,680);
+  score-= 50;
+
+
+}{
+  /// start sun
+  sunX=width*3/4;
+  sunY=100;
+  
+}
+
+//// NEXT FRAME:  scene, action, show.
 void draw() {
-  background( 250,250,200 );
-  rectMode( CORNERS );
-  table( left, top, right, bottom );
-  bounce();
   collisions();
-  show();
+  //// SCENE:  sky, sun, tree, house, etc.
+  background( 100,200,250 );                // sky
+  fill( 255,255,0 );
+  sunX= (sunX+1) % (width+150);
  
-}
-//// SCENE:  draw the table with walls
-void table( float left, float top, float right, float bottom ) {
-  fill( 50, 205, 50 );    // green pool table
-  strokeWeight(20);
-  stroke( 139, 69, 19 );      // Brown walls
-  rect( left-25, top-25, right+25, bottom+25 );
-  stroke(0);
-  strokeWeight(1);
-}
-
-//// ACTION:  bounce off walls, collisions
-void bounce() {
-  redX += redDX;  if ( redX<left || redX>right ) redDX *= -1;
-  redY += redDY;  if ( redY<top || redY>bottom ) redDY *=  -1;
-  yelX += yelDX;  if ( yelX<left || yelX>right ) yelDX *= -1;
-  yelY += yelDY;  if ( yelY<top || yelY>bottom ) yelDY *=  -1;
-  bluX += bluDX;  if ( bluX<left || bluX>right ) bluDX *= -1;
-  bluY += bluDY;  if ( bluY<top || bluY>bottom ) bluDY *=  -1;
-  if (cueDX != 0);
-  cueX += cueDX;  if ( cueX<left || cueX>right ) cueDX *= -1;
-  cueY += cueDY;  if ( cueY<top || cueY>bottom ) cueDY *=  -1;
+  ellipse( sunX,sunY, 40,40 );    // sun
+  
+  fill( 100,200,100 );
+  rect( 0,horizon, width,height*3/4 );      // grass.
+  
+  triangle(550,150,600,150,575,100);
+  fill(139,80,13);
+  rect(568,150,15,25);
   
   
-}
-void collisions() {
-  float tmp;
-  // Swap velocities!
-  if ( dist( redX,redY, yelX,yelY ) < 30 ) {
-    tmp=yelDX;  yelDX=redDX;  redDX=tmp;
-    tmp=yelDY;  yelDY=redDY;  redDY=tmp;
-  }
-  if ( dist( redX,redY, bluX,bluY) < 30 ) {
-    tmp=bluDX;  bluDX=redDX;  redDX=tmp;
-    tmp=bluDY;  bluDY=redDY;  redDY=tmp;
-  }
-  if ( dist( bluX,bluY, yelX, yelY) < 30 ) {
-    tmp=yelDX;  yelDX=bluDX;  bluDX=tmp;
-    tmp=yelDY;  yelDY=bluDY;  bluDY=tmp;
-  }
-  if (cueDX != 0){ 
-  if ( dist( bluX,bluY, cueX, cueY) < 30 ) {
-    tmp=cueDX;  cueDX=bluDX;  bluDX=tmp;
-    tmp=cueDY;  cueDY=bluDY;  bluDY=tmp;
-  }
-  if ( dist( cueX, cueY, redX,redY) < 30 ) {
-    tmp=redDX;  redDX=cueDX;  cueDX=tmp;
-    tmp=redDY;  redDY=cueDY;  cueDY=tmp;
-  
-}
- if( dist( cueX, cueY, yelX,yelY) < 30 ) {
-    tmp=yelDX;  yelDX=cueDX;  cueDX=tmp;
-    tmp=yelDY;  yelDY=cueDY;  cueDY=tmp;
-} 
-}}
-
-//// SHOW:  balls, messages
-void show() {
-  fill( 255,255,255 );    ellipse( redX,redY, 30,30 );
-  fill( 255,0,0 );    ellipse( redX,redY, 30,30 );
-  fill( 255,255,0 );  ellipse( yelX,yelY, 30,30 );
-  fill( 0,0,255 );    ellipse( bluX,bluY, 30,30 );
-  fill( 255,255,255 );    ellipse( cueX,cueY, 30,30 );
-}
-void messages() {
+  fill(255,0,0);
+  rect( 150,75, 90,100);  // house
+  fill(139,80,13);
+  rect(200,125,25,50);
   fill(0);
-  text( title, width/3, 20 );
-  text( news, width/3, 40 );
-  text( author, 10, height-10 );
+  triangle(151,75,240,75,196,20);
+  fill(255);
+  rect(155,90,30,30);
+  rect(205,90,30,30);
+  
+  //Scoreing
+  textSize(20);
+  fill(0);
+  text("Score",50,50);
+  text(score,100,50);
+  
+                                      
+
+  fill(0);                //INFO
+  textSize(16);
+  text( "Drake", 10,height-20 );
+  text("'r' to restart",175,height-20);
+  text("'q' to quit",400,height-20);
+  textSize(12);  
+                                            
+ //// ACTION:  move (x,y) coordinates
+ 
+ dx=0; 
+ dy=0;
+  
+ if (keyPressed && (key == 'W' || key == 'w') ){
+      dy = -1; 
+ }
+ 
+ 
+   
+ if (keyPressed) {
+    if (key == 'S' || key == 's') 
+    {
+      dy = +1; 
+    }
+ }
+
+ 
+ 
+  if (keyPressed) {
+    if (key == 'A' || key == 'a') {
+      dx = -1; 
+    }
+ }
+ 
+ if (keyPressed) {
+   if (key == 'D' || key == 'd') {
+    dx = 1; }
+ }
+ 
+ X= X + DX;
+ Y= Y + DY;
+ 
+ {
+   X += DX; if( X<left || X+25 >right)DX *= -1;
+   Y += DY; if( Y-15< horizon ||Y+50>bottom) DY *= -1;
+ }
+ 
+    
+  
+  //Hero
+  fill(50,80,150);    // body
+  rect( x+3,y, 25,50,10 );
+  fill(238,206,179);   // head
+  ellipse(x+15,y-11,20,20);  
+  fill(0,0,155);    //eyes
+  ellipse(x+12,y-13,3,3);
+  ellipse(x+18,y-13,3,3);
+  fill(255,255,0);
+  stroke(0);
+  fill(255);
+  textSize(9);
+  text( "Jimmy", x+3,y+30 );
+  
+  x += dx;
+  y += dy;
+  
+    //Monster 1
+  rect(X,Y,30,70,10);
+  ellipse(X+15,Y-15,35,35);
+
+    //Coin (gold)
+  fill(255,255,0);
+  ellipse(coinx,coiny,10,10);
 }
 
+void collisions(){
+  //if( 120 < X < horizon-50) { DX = -DX;}
+  if( dist ( x,y-15,X,Y-60) < 30) {reset();}
+  if( dist ( x,y+15,X,Y+70) < 30) {reset();}
+}
 
-//// HANDLERS:  keys, click
+//////// HANDLERS:  mouse clicks, keys
+void mousePressed() {
+  x=  mouseX;                             // Set (x,y) to mouse
+  y=  mouseY;
+  //
+ // dx=  random( -6, +6 );                  // random speed.
+  //dy=  random( -4, +4 );
+}
+
 void keyPressed() {
-  if (key == 'r') {
-    reset();
-  }
+  if (key == 'q') {
+    exit();                           // press 'q' key to QUIT.
+  }                                   // press 'r' to RESET
+  {
+    if (key == 'r') {   
+      reset();
+    }}
 }
+   
